9da87b57e61fc537fc6728737b74af69237c149f
jorge garcia <jorge.garcia.celis@gmail.com>
2024-09-27 09:25:14 -0600

    first commit

---
+++ content-script.js
@@ -0,0 +1,39 @@
+// Main function to execute the code
+function executeCode() {
+
+    // Enable right-click
+    document.oncontextmenu = function () {
+        return true;
+    };
+
+    // Enable right mouse button
+    document.addEventListener("mousedown", function (e) {
+        if (e.button === 2 || e.button === 0) {
+            return true;
+        }
+    });
+
+    // Function to allow copy and paste
+    function allowCopyAndPaste(e) {
+        e.stopImmediatePropagation();
+        return true;
+    }
+
+    //Delete onpaste event listener
+    document.querySelectorAll("[onpaste]").forEach(function (element) {
+        element.removeAttribute("onpaste");
+    });
+
+    // Events for copy and paste
+    document.addEventListener("copy", allowCopyAndPaste, true);
+    document.addEventListener("paste", allowCopyAndPaste, true);
+
+    // Enable Ctrl+V
+    document.addEventListener("keydown", function (e) {
+        allowCopyAndPaste(e);
+    });
+
+    console.log("Copy-Paste Enabled")
+}
+
+setInterval(executeCode, 10000);
